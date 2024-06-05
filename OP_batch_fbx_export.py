8e41b0f83026ccd7454c3994a98c80d307f111da
Henri Hebeisen <henri.hebeisen@gmail.com>
2024-06-05 23:23:21 +0200

    added more flexibilty for action prefixes, might need to move option to preferences

--- OP_batch_fbx_export.py
+++ OP_batch_fbx_export.py
@@ -1,17 +1,32 @@
 import bpy
 import time
+import re
 from bpy.types import Operator
-from bpy.props import IntProperty
+from bpy.props import StringProperty, BoolProperty
 from addon_utils import enable
 from os import path
 
 from .utils import get_addon_prefs
+
+
+
 
 class IO_OT_BatchExportFBX(Operator):
     """Export every animation in a separate FBX"""
     bl_idname = "io.batch_export_fbx"
     bl_label = "Export all animation"
     bl_options = {'REGISTER','UNDO'}
+
+    usePrefix : BoolProperty(
+        name="Use Action Prefix",
+        default=False
+    )
+
+    animation_prefixes : StringProperty(
+        name="Action Prefixes",
+        default="ac, cy",
+        description="actions that start with these value will be exported"
+    )
 
     @classmethod
     def poll(cls, context):
@@ -24,6 +39,8 @@ class IO_OT_BatchExportFBX(Operator):
         if not success:
             self.report({'WARNING'}, "Please enable the better fbx addon ! ")
             return {'CANCELLED'}
+        else:
+            print("better fbx is installed, let's export some animation")
 
 
         rel_path = get_addon_prefs().export_folder_default
@@ -34,24 +51,27 @@ class IO_OT_BatchExportFBX(Operator):
         current_action = obj.animation_data.action
 
         for action in bpy.data.actions: 
-            if action.name.startswith('ac') or action.name.startswith('cy'):
-                obj.animation_data.action = action
-                file_name = f"{obj.name}_{action.name}.fbx"
+            if self.usePrefix :
+                prefixes  = self.animation_prefixes.replace(",","|").replace(" ", "")
+                if not re.match(f"^({prefixes})",action.name):
+                    continue
 
-                file_path = path.join(abs_path, file_name)
-                start = time.time()
+            obj.animation_data.action = action
+            file_name = f"{obj.name}_{action.name}.fbx"
 
+            file_path = path.join(abs_path, file_name)
+            start = time.time()
 
-                bpy.ops.better_export.fbx(filepath=file_path, 
-                                          my_fbx_axis='Unity', 
-                                          use_selection=True,
-                                          use_only_deform_bones=True,
-                                          use_animation=True,
-                                          use_export_materials=False,
-                                          my_animation_type='Active')
-                
-                end = time.time()
-                print(f"Export for {file_name} done in {end-start}s")
+            bpy.ops.better_export.fbx(filepath=file_path, 
+                                        my_fbx_axis='Unity', 
+                                        use_selection=True,
+                                        use_only_deform_bones=True,
+                                        use_animation=True,
+                                        use_export_materials=False,
+                                        my_animation_type='Active')
+            
+            end = time.time()
+            print(f"Export for {file_name} done in {end-start}s")
 
 
         #we restore the previous current_action
